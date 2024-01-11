baa425f9857b53e1675d0f9a51963f308aa68e87
Developer-RONNIE <ronnieworkspace@gmail.com>
2024-01-12 04:56:38 +0530

    complete sign in page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
-export default function Signup() {
+export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
